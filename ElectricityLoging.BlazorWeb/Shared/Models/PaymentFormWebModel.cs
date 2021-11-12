0d2e16eae82d816ad0de22be3cbc9e2bb0b4fbe6
tenchi69 <tenchi69@gmail.com>
2021-11-13 00:29:26 +0200

    Created basic input form with web model

---
+++ ElectricityLoging.BlazorWeb/Shared/Models/PaymentFormWebModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityLoging.BlazorWeb.Shared.Models
+{
+    public class PaymentFormWebModel
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime PaymentDay { get; set; }
+        public decimal ElectricityPrice { get; set; }
+        public int LastMeterNumber { get; set; }
+        public int CurrentMeterNumber { get; set; }
+        public int Difference { get; set; }
+        public decimal Debt { get; set; }
+        public decimal AmountPayed { get; set; }
+
+        public PaymentFormWebModel()
+        {
+            DateCreated = DateTime.Now;
+            PaymentDay = DateTime.Now;
+        }
+    }
+}
